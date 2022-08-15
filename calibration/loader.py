5754d4a8f9a9df8000b0d298cfc8b00ce87cd31d
mfbenitezp <fbenitez@turing.ac.uk>
2022-08-15 16:51:07 +0100

    making progress with the code for multiprocesing and aspcis

---
+++ calibration/loader.py
@@ -0,0 +1,123 @@
+import os
+import numpy as np
+from yaml import load, SafeLoader
+
+from simulator import Simulator
+from aspics.snapshot import Snapshot
+from aspics.params import Params, IndividualHazardMultipliers, LocationHazardMultipliers
+
+
+def setup_sim_from_file(parameters_file):
+    print(f"Running a simulation based on {parameters_file}")
+    with open(parameters_file, "r") as f:
+        parameters = load(f, Loader=SafeLoader)
+        return setup_sim(parameters)
+
+
+def setup_sim(parameters):
+    print(f"Running a manually added parameters simulation based on {parameters}")
+
+    sim_params = parameters["microsim"]
+    calibration_params = parameters["microsim_calibration"]
+    disease_params = parameters["disease"]
+    iterations = sim_params["iterations"]
+    study_area = sim_params["study-area"]
+    output = sim_params["output"]
+    output_every_iteration = sim_params["output-every-iteration"]
+    use_lockdown = sim_params["use-lockdown"]
+    start_date = sim_params["start-date"]
+
+    # Check the parameters are sensible
+    if iterations < 1:
+        raise ValueError("Iterations must be > 1")
+    if (not output) and output_every_iteration:
+        raise ValueError(
+            "Can't choose to not output any data (output=False) but also write the data at every "
+            "iteration (output_every_iteration=True)"
+        )
+
+    # Load the snapshot file
+    snapshot_path = f"../data/snapshots/{study_area}/cache.npz"
+    if not os.path.exists(snapshot_path):
+        raise Exception(
+            f"Missing snapshot cache {snapshot_path}. Run SPC and convert_snapshot.py first to generate it."
+        )
+    print(f"Loading snapshot from {snapshot_path}")
+    snapshot = Snapshot.load_full_snapshot(path=snapshot_path)
+    print(f"Snapshot is {int(snapshot.num_bytes() / 1000000)} MB")
+
+    # Apply lockdown values
+    if use_lockdown:
+        # Skip past the first entries
+        snapshot.lockdown_multipliers = snapshot.lockdown_multipliers[start_date:]
+    else:
+        # No lockdown
+        snapshot.lockdown_multipliers = np.ones(iterations + 1)
+
+    # set the random seed of the model
+    snapshot.seed_prngs(42)
+
+    # set params
+    if calibration_params is not None and disease_params is not None:
+        snapshot.update_params(create_params(calibration_params, disease_params))
+
+        if disease_params["improve_health"]:
+            print("Switching to healthier population")
+            snapshot.switch_to_healthier_population()
+
+    # Create a simulator and upload the snapshot data to the OpenCL device
+    simulator = Simulator(snapshot, study_area, gpu=True)
+    [people_statuses, people_transition_times] = simulator.seeding_base()
+    simulator.upload_all(snapshot.buffers)
+    simulator.upload("people_statuses", people_statuses)
+    simulator.upload("people_transition_times", people_transition_times)
+
+    return simulator, snapshot, study_area, iterations
+
+
+def create_params(calibration_params, disease_params):
+
+    current_risk_beta = disease_params["current_risk_beta"]
+    # NB: OpenCL model incorporates the current risk beta by pre-multiplying the hazard multipliers with it
+    location_hazard_multipliers = LocationHazardMultipliers(
+        retail=calibration_params["hazard_location_multipliers"]["Retail"]
+               * current_risk_beta,
+        primary_school=calibration_params["hazard_location_multipliers"][
+                           "PrimarySchool"
+                       ]
+                       * current_risk_beta,
+        secondary_school=calibration_params["hazard_location_multipliers"][
+                             "SecondarySchool"
+                         ]
+                         * current_risk_beta,
+        home=calibration_params["hazard_location_multipliers"]["Home"]
+             * current_risk_beta,
+        work=calibration_params["hazard_location_multipliers"]["Work"]
+             * current_risk_beta,
+    )
+
+    individual_hazard_multipliers = IndividualHazardMultipliers(
+        presymptomatic=calibration_params["hazard_individual_multipliers"][
+            "presymptomatic"
+        ],
+        asymptomatic=calibration_params["hazard_individual_multipliers"][
+            "asymptomatic"
+        ],
+        symptomatic=calibration_params["hazard_individual_multipliers"]["symptomatic"],
+    )
+
+    obesity_multipliers = [
+        disease_params["overweight"],
+        disease_params["obesity_30"],
+        disease_params["obesity_35"],
+        disease_params["obesity_40"],
+    ]
+
+    return Params(
+        location_hazard_multipliers=location_hazard_multipliers,
+        individual_hazard_multipliers=individual_hazard_multipliers,
+        obesity_multipliers=obesity_multipliers,
+        cvd_multiplier=disease_params["cvd"],
+        diabetes_multiplier=disease_params["diabetes"],
+        bloodpressure_multiplier=disease_params["bloodpressure"],
+    )
--- calibration/run_aspics.py
+++ calibration/run_aspics.py
@@ -18,38 +18,39 @@ from yaml import load, SafeLoader
 sys.path.append('../')
 from headless import run_headless
 from typing import List
-from simulator import Simulator  # Not an elegant way to address path issues.
+from aspics.simulator import Simulator  # Not an elegant way to address path issues.
 from aspics.snapshot import Snapshot
 from aspics.loader import create_params
 from aspics.params import Params, IndividualHazardMultipliers, LocationHazardMultipliers
 from aspics.disease_statuses import DiseaseStatus
 from aspics.summary import Summary
 
-class OpenCLRunner:
 
+class OpenCLRunner:
     constants = {}
 
     @classmethod
     def init(cls,
-             iterations: int,
-             repetitions: int,
              study_area: str,
+             iterations: int,
+             use_lockdown: bool,
+             start_date: str,
+             repetitions: int,
              observations: pd.DataFrame,
              use_gpu: bool,
              use_healthier_pop: bool,
              store_detailed_counts: bool,
              parameters_file: str,
-             snapshot_filepath: str):
+             ):
 
+        cls.STUDY_AREA = study_area
         cls.ITERATIONS = iterations
         cls.REPETITIONS = repetitions
-        cls.STUDY_AREA = study_area
         cls.OBSERVATIONS = observations
         cls.USE_GPU = use_gpu
         cls.USE_HEALTHIER_POP = use_healthier_pop
         cls.STORE_DETAILED_COUNTS = store_detailed_counts
         cls.PARAMETERS_FILE = parameters_file
-        cls.SNAPSHOT_FILEPATH = snapshot_filepath
         cls.initialised = True
 
     @classmethod
@@ -62,7 +63,7 @@ class OpenCLRunner:
                use_healthier_pop=None,
                store_detailed_counts: bool = None,
                parameters_file: str = None,
-               snapshot_filepath: str = None):
+               ):
         """
         Update any of the variables that have already been initialised
         """
@@ -84,8 +85,6 @@ class OpenCLRunner:
             cls.STORE_DETAILED_COUNTS = store_detailed_counts
         if parameters_file is not None:
             cls.PARAMETERS_FILE = parameters_file
-        if snapshot_filepath is not None:
-            cls.SNAPSHOT_FILEPATH = snapshot_filepath
 
     @classmethod  ### To ask Nick.
     def set_constants(cls, constants):
@@ -209,40 +208,40 @@ class OpenCLRunner:
 
     @staticmethod
     def create_params_manually(
+            # Microsim params
             parameters_file: str = None,
-            study_area : str = None,
+            study_area: str = None,
+            # dissease Params
             current_risk_beta: float = None,
             infection_log_scale: float = None,
             infection_mode: float = None,
+            # hazard_individual_multipliers_params
             presymptomatic: float = None,
             asymptomatic: float = None,
             symptomatic: float = None,
+            # hazard_location_multipliers_params
             retail: float = None,
             primary_school: float = None,
             secondary_school: float = None,
             home: float = None,
             work: float = None,
-            ):
+    ):
 
         print(f"Creating parameters manually based on values from Notebook")
 
         # Read the default parameters from a yml file, then override with any provided
         if parameters_file is None:
-            parameters_file = f"../config/{study_area}.yml"
+            parameters_file = f"config/{study_area}.yml"
+            print(f"I read the parameter file '{study_area}")
         elif not os.path.isfile(parameters_file):
             raise Exception(f"The given parameters file '{parameters_file} is not a file, check the /config folder")
 
         with open(parameters_file) as f:
             parameters = yaml.load(f, Loader=yaml.SafeLoader)
 
-        ################
-        ### Check_None function to validate the parameters, based on Nick's work
-        ##################
-
-        sim_params = parameters["microsim"]  # Parameters for the dynamic microsim (python)
+        # sim_params = parameters["microsim"]  # Parameters for the dynamic microsim (python)
         calibration_params = parameters["microsim_calibration"]
         disease_params = parameters["disease"]  # Parameters for the disease model (r)
-
 
         # current_risk_beta needs to be set first  as the OpenCL model pre-multiplies the hazard multipliers by it
         current_risk_beta = OpenCLRunner._check_if_none("current_risk_beta",
@@ -402,19 +401,32 @@ class OpenCLRunner:
         return simulator, snapshot, study_area, iterations
 
     @staticmethod
-    def run_aspics_opencl(self: int,
-                          parameters_file,
-                          iterations: int,
-                          params,
-                          use_gpu: bool,
-                          use_healthier_pop: bool,
-                          store_detailed_counts: bool = True,
-                          quiet=False) -> (np.ndarray, np.ndarray):
+    def run_aspics(i: int,
+                   iterations: int,
+                   study_area: str,
+                   params,
+                   output: bool,
+                   output_every_iteration: bool,
+                   use_gpu: bool,
+                   use_lockdown: bool,
+                   start_date: int,
+                   calibration_params,
+                   disease_params,
+                   store_detailed_counts: bool = True,
+                   quiet=False) -> (np.ndarray, np.ndarray):
 
-        print(f"Running a simulation ", {i}," based on the study area")
+        print(f"Running a simulation ", {i}, " based on the study area")
+        # Check the parameters are sensible
+        if iterations < 1:
+            raise ValueError("Iterations must be > 1")
+        if (not output) and output_every_iteration:
+            raise ValueError(
+                "Can't choose to not output any data (output=False) but also write the data at every "
+                "iteration (output_every_iteration=True)"
+            )
 
-        # 1.  Load the snapshot file
-        snapshot_path = f"../data/snapshots/{study_area}/cache.npz"
+        # Load the snapshot file
+        snapshot_path = f"data/snapshots/{study_area}/cache.npz"
         if not os.path.exists(snapshot_path):
             raise Exception(
                 f"Missing snapshot cache {snapshot_path}. Run SPC and convert_snapshot.py first to generate it."
@@ -423,63 +435,75 @@ class OpenCLRunner:
         snapshot = Snapshot.load_full_snapshot(path=snapshot_path)
         print(f"Snapshot is {int(snapshot.num_bytes() / 1000000)} MB")
 
-        prev_obesity = np.copy(snapshot.buffers.people_obesity)
-        if use_healthier_pop:
-            snapshot.switch_to_healthier_population()
+        # Apply lockdown values
+        if use_lockdown:
+            # Skip past the first entries
+            snapshot.lockdown_multipliers = snapshot.lockdown_multipliers[start_date:]
+        else:
+            # No lockdown
+            snapshot.lockdown_multipliers = np.ones(iterations + 1)
 
-        print("testing obesity arrays not equal")
-        print(np.mean(prev_obesity))
-        print(np.mean(snapshot.buffers.people_obesity))
-
-        # 2. set the random seed of the model for each repetition, otherwise it is completely deterministic
+        # set the random seed of the model
         snapshot.seed_prngs(i)
 
-        # 3. set params for snapshot, based on params
-        snapshot.update_params(params)
+        # set params
+        if calibration_params is not None and disease_params is not None:
+            snapshot.update_params(create_params(calibration_params, disease_params))
+
+            if disease_params["improve_health"]:
+                print("Switching to healthier population")
+                snapshot.switch_to_healthier_population()
 
         # Create a simulator and upload the snapshot data to the OpenCL device
-        simulator = Simulator(snapshot, parameters_file, gpu=True)
-
-        # Ask Hadrien about this new menthod of seeding
+        simulator = Simulator(snapshot, study_area, gpu=True)
         [people_statuses, people_transition_times] = simulator.seeding_base()
-        # Upload the snapshot
         simulator.upload_all(snapshot.buffers)
         simulator.upload("people_statuses", people_statuses)
         simulator.upload("people_transition_times", people_transition_times)
 
-        if not quiet:
-            print(f"Running simulation {self + 1}.")
-        summary, final_state = run_headless(simulator, snapshot, iterations, quiet=True,
+        summary, final_state = run_headless(simulator, snapshot, iterations, quiet,
                                             store_detailed_counts=store_detailed_counts)
         return summary, final_state
 
     @staticmethod
-    def run_aspics_opencl_multi(
+    def run_aspics_multi(
             repetitions: int,
             iterations: int,
-            params: Params,
-            use_gpu: bool = False,
-            use_healthier_pop: bool = False,
+            study_area: str,
+            params,
+            output: bool,
+            output_every_iteration: bool,
+            use_gpu: bool,
+            use_lockdown: bool,
+            start_date: int,
+            calibration_params,
+            disease_params,
             store_detailed_counts: bool = False,
-            #opencl_dir=os.path.join(".", "microsim", "opencl"),
-            #snapshot_filepath=os.path.join(".", "microsim", "opencl", "snapshots", "cache.npz"),
+            quiet=False,
             multiprocess=False,
             random_ids=False
-            ):
+    ):
 
         # Prepare the function arguments. We need one set of arguments per repetition
         l_i = [i for i in range(repetitions)] if not random_ids else \
             [random.randint(1, 100000) for _ in range(repetitions)]
         l_iterations = [iterations] * repetitions
-        #l_snapshot_filepath = [snapshot_filepath] * repetitions
+        l_study_area = [study_area] * repetitions
         l_params = [params] * repetitions
-        #l_opencl_dir = [opencl_dir] * repetitions
+        l_output = [output] * repetitions
+        l_output_every_iteration = [output_every_iteration] * repetitions
         l_use_gpu = [use_gpu] * repetitions
-        l_use_healthier_pop = [use_healthier_pop] * repetitions
+        l_use_lockdown = [use_lockdown] * repetitions
+        l_start_date = [start_date] * repetitions
+        l_calibration_params = [calibration_params] * repetitions
+        l_disease_params = [disease_params] * repetitions
+        # l_use_healthier_pop = [use_healthier_pop] * repetitions
         l_store_detailed_counts = [store_detailed_counts] * repetitions
         l_quiet = [False] * repetitions  # Don't print info
 
-        args = zip(l_i, l_iterations, l_params, l_use_gpu, l_use_healthier_pop, l_store_detailed_counts, l_quiet)
+        args = zip(l_i, l_iterations, l_study_area, l_params, l_output, l_output_every_iteration, l_use_gpu,
+                   l_use_lockdown,
+                   l_start_date, l_calibration_params, l_disease_params, l_store_detailed_counts, l_quiet)
         to_return = None
         start_time = time.time()
 
@@ -487,11 +511,11 @@ class OpenCLRunner:
             try:
                 print("Running multiple models in multiprocess mode ... ", end="", flush=True)
                 with multiprocessing.Pool(processes=int(os.cpu_count())) as pool:
-                    to_return = pool.starmap(OpenCLRunner.run_aspics_opencl, args)
+                    to_return = pool.starmap(OpenCLRunner.run_aspics, args)
             finally:  # Make sure they get closed (shouldn't be necessary)
                 pool.close()
         else:
-            results = itertools.starmap(OpenCLRunner.run_aspics_opencl, args)
+            results = itertools.starmap(OpenCLRunner.run_aspics, args)
             # Return as a list to force the models to execute (otherwise this is delayed because starmap returns
             # a generator. Also means we can use tqdm to get a progress bar, which is nice.
             to_return = [x for x in tqdm.tqdm(results, desc="Running models", total=repetitions)]
@@ -501,11 +525,90 @@ class OpenCLRunner:
         return to_return
 
     @classmethod
+    def run_aspics_with_params_abc(cls, input_params_dict: dict, return_full_details=False, quiet=True):
+
+        # print("opencl_runner.py -- run_model_with_params_abc")
+        if not cls.initialised:
+            raise Exception("The OpenCLRunner class needs to be initialised first. "
+                            "Call the OpenCLRunner.init() function")
+
+        # Check that all input parameters are not negative
+        for k, v in input_params_dict.items():
+            if v < 0:
+                raise Exception(f"The parameter {k}={v} < 0. "
+                                f"All parameters: {input_params_dict}")
+
+        # Splat the input_params_dict to automatically set any parameters that have been inlcluded
+        params = OpenCLRunner.create_params_manually(parameters_file=cls.PARAMETERS_FILE, **input_params_dict)
+
+        # Run the model
+        results = OpenCLRunner.OpenCLRunner.run_aspics_multi(
+            repetitions=cls.REPETITIONS, iterations=cls.ITERATIONS, params=params, start_date=cls.START_DAY,
+            quiet=quiet , use_gpu=cls.USE_GPU,
+            store_detailed_counts=cls.STORE_DETAILED_COUNTS, multiprocess=False, random_ids=True)
+
+        # Create summary objects containing the model results
+        summaries = [x[0] for x in results]
+
+        ########################################################################
+        ########################################################################
+        # Create arrays containing the modelled number of cases on each day/week
+        ########################################################################
+        ########################################################################
+        # Get the cumulative number of infections per day (i.e. simulated results)
+        # i.e the total number of cases there has been up to this point, for each day
+        model_daily_cumulative_infections = OpenCLRunner.get_cumulative_daily_infections(summaries)
+
+        ##### Convert this to the cumulative number of infections at the end of each week
+        # Get the number of days the model was ran for
+        n_days = len(summaries[0].total_counts[DiseaseStatus.Exposed.value])
+        # List the index matching the end day of each week (e.g. 7, 14, 21 etc (minus 1 for indexing starting at 0)
+        week_end_days = list(range(6,n_days+1,7))
+        # Get the weekly cumulative total
+        model_weekly_cumulative_infections = model_daily_cumulative_infections[week_end_days]
+
+        ########################################################################
+        ########################################################################
+        # Create array containing the cumulatuve number of positive test
+        # results there have been so far observed on each day
+        ########################################################################
+        ########################################################################
+        # Get the observations
+        obs_weekly_cumulative_infections = cls.OBSERVATIONS
+
+        # Keep only as many weeks of data as are in the model results
+        n_weeks = int(n_days / 7)
+        obs_weekly_cumulative_infections = obs_weekly_cumulative_infections[0:n_weeks]
+
+        # find the distance between the vector of weekly values
+        distance = OpenCLRunner.fit_l2(obs_weekly_cumulative_infections, model_weekly_cumulative_infections)
+
+        ########################################################################
+        ########################################################################
+        # Return model results
+        ########################################################################
+        ########################################################################
+        if return_full_details:
+            # check same length (but obviously will be now as set length based on model)
+            assert len(model_weekly_cumulative_infections) == len(obs_weekly_cumulative_infections)
+            # Can compare these to the observations to get a fitness
+            return distance, model_weekly_cumulative_infections, obs_weekly_cumulative_infections, params, summaries
+        else:  # Return the expected counts in a dictionary
+            return {"data": model_weekly_cumulative_infections, "data_daily":model_daily_cumulative_infections, 'distance':distance,
+                    'params': params}
+
+
+
+    """
+    Functions from opencl_runner from
+    """
+
+    @classmethod
     def run_aspics_opencl_params(
             cls,
             input_params: List,
             return_full_details=False
-            ):
+    ):
         if not cls.initialised:
             raise Exception("The OpenCLRunner class needs to be initialised first. "
                             "Call the OpenCLRunner.init() function")
@@ -529,10 +632,10 @@ class OpenCLRunner:
         results = OpenCLRunner.run_aspics_opencl_multi(
             repetitions=cls.REPETITIONS,
             iterations=cls.ITERATIONS,
-            parameters_file= cls.PARAMETERS_FILE,
+            parameters_file=cls.PARAMETERS_FILE,
             params=params,
-            #opencl_dir=cls.OPENCL_DIR,
-            #snapshot_filepath=cls.SNAPSHOT_FILEPATH,
+            # opencl_dir=cls.OPENCL_DIR,
+            # snapshot_filepath=cls.SNAPSHOT_FILEPATH,
             use_gpu=cls.USE_GPU,
             store_detailed_counts=cls.STORE_DETAILED_COUNTS,
             multiprocess=False
@@ -576,10 +679,10 @@ class OpenCLRunner:
         results = OpenCLRunner.run_aspics_opencl_multi(
             repetitions=cls.REPETITIONS,
             iterations=cls.ITERATIONS,
-            parameters_file= cls.PARAMETERS_FILE,
+            parameters_file=cls.PARAMETERS_FILE,
             params=params,
-            #opencl_dir=cls.OPENCL_DIR,
-            #snapshot_filepath=cls.SNAPSHOT_FILEPATH,
+            # opencl_dir=cls.OPENCL_DIR,
+            # snapshot_filepath=cls.SNAPSHOT_FILEPATH,
             use_gpu=cls.USE_GPU,
             store_detailed_counts=cls.STORE_DETAILED_COUNTS,
             multiprocess=False,
